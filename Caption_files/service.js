95f8ab480c89af8ab0c1afbb557f1da62f9472a9
Elijah Butterfield <elijah.code@gmail.com>
2014-09-20 02:05:26 -0700

    Added Raw Files From Appery

---
+++ Caption_files/service.js
@@ -0,0 +1,39 @@
+/*
+ * Services
+ */
+
+var RESTService = new Apperyio.RestService({
+    'url': '',
+    'dataType': 'json',
+    'type': 'get',
+});
+var CameraService = new Apperyio.CameraService({});
+/*
+ * Data models
+ */
+Apperyio.models = {
+    "AAB7DD8B-612F-6E85-DA32-0D74E1330AA0": {
+        "name": "String",
+        "type": "string",
+        "parentGuid": null,
+        "reference": null
+    },
+    "ABB7DD8B-612F-6E85-DA32-0D74E1330AA1": {
+        "name": "Number",
+        "type": "number",
+        "parentGuid": null,
+        "reference": null
+    },
+    "ACB7DD8B-612F-6E85-DA32-0D74E1330AA2": {
+        "name": "Boolean",
+        "type": "boolean",
+        "parentGuid": null,
+        "reference": null
+    }
+
+};
+
+/*
+ * Data storages
+ */
+Apperyio.storages = {};
